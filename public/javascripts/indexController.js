03ebf55912eca179c919e4d223372ab3e191348c
bekirytm <bekir.yetim.06@gmail.com>
2020-07-09 07:06:42 +0300

    socket io

--- public/javascripts/indexController.js
+++ public/javascripts/indexController.js
@@ -1,3 +1,3 @@
 app.controller('indexController' , ['$scope' , ($scope) => {        //Bunu da sayfamıza dahil ediyoruz.(layout.pug)
-    console.log("Merhaba");
+    const socket = io.connect('http://localhost:3000');
 }]);
